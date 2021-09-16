2b5ecd2f360856c97972ca343a2100372e531f15
vogets <archsameerudemy@gmail.com>
2021-09-16 19:43:29 +0530

    Created Basic Structure. Closes #1

---
+++ src/main/java/com/bdt/asmy/AsmyApplication.java
@@ -0,0 +1,13 @@
+package com.bdt.asmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bdt/asmy/AsmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdt.asmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
